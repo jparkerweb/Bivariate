bc5aeab835084b7cfb33b9a19f54b41811bdb973
Justin Parker <jparkerweb@gmail.com>
2016-07-06 14:24:00 -0700

    swap ascii logo colors ;)

--- app_modules/_asciiLogo.js
+++ app_modules/_asciiLogo.js
@@ -1,11 +1,11 @@
 var asciiLogo = function() {
-    console.log(('______ _                 _       _       ').bgGreen.white.bold);
-    console.log(('| ___ (_)               (_)     | |      ').bgGreen.white.bold);
-    console.log(('| |_/ /___   ____ _ _ __ _  __ _| |_ ___ ').bgGreen.white.bold);
-    console.log(('| ___ \\ \\ \\ / / _` | \'__| |/ _` | __/ _ \\').bgGreen.white.bold);
-    console.log(('| |_/ / |\\ V / (_| | |  | | (_| | ||  __/').bgCyan.white.bold);
-    console.log(('\\____/|_| \\_/ \\__,_|_|  |_|\\__,_|\\__\\___|').bgCyan.white.bold);
-    console.log(('                                         ').bgCyan.white.bold);
+    console.log(('______ _                 _       _       ').bgCyan.white.bold);
+    console.log(('| ___ (_)               (_)     | |      ').bgCyan.white.bold);
+    console.log(('| |_/ /___   ____ _ _ __ _  __ _| |_ ___ ').bgCyan.white.bold);
+    console.log(('| ___ \\ \\ \\ / / _` | \'__| |/ _` | __/ _ \\').bgCyan.white.bold);
+    console.log(('| |_/ / |\\ V / (_| | |  | | (_| | ||  __/').bgGreen.white.bold);
+    console.log(('\\____/|_| \\_/ \\__,_|_|  |_|\\__,_|\\__\\___|').bgGreen.white.bold);
+    console.log(('                                         ').bgGreen.white.bold);
 };
 
 module.exports = asciiLogo;
