7a7f8c40fd98ab3da913bb2f99cb1ee91c9cb1a5
jparkerweb <jparkerweb@gmail.com>
2018-08-14 10:02:36 -0700

    remove unneeded semicolons and add commented example page interactions

--- init-bivariate-data/templates/onReady-example.js
+++ init-bivariate-data/templates/onReady-example.js
@@ -1,9 +1,10 @@
 module.exports = function (chromy, scenario, vp) {
-	console.log('onReady-_____name_____.js is running for: ', vp.label);
+	console.log('onReady-_____name_____.js is running for: ', vp.label)
 	
-	return chromy
-		.evaluate(function() {
-			// page interactions here
-			console.log('This custom script is running inside your web app!');
-		});
-};
+	// return chromy
+	// 	.wait(".some-element")
+	// 	.evaluate(function() {
+	// 		console.log('This custom script is running inside your web app!')
+	// 		document.querySelector(".some-button").click()
+	// 	});
+}
