b07332d7539c0084d26c07ea24210af674f52b2e
jparkerweb <jparkerweb@gmail.com>
2018-09-06 08:47:03 -0700

    update line endings and create test template comments

--- init-bivariate-data/templates/_example.js
+++ init-bivariate-data/templates/_example.js
@@ -7,44 +7,33 @@
 //   related tests in named subdirectories
 //   for better organization
 
-var label = "_____label_____"
-var route = "/_____route_____"
-var onBeforeScript = null
-var onReadyScript = "_____onReadyScriptName_____"
-var readySelector = "_____readySelector_____"
-var delay = _____delay_____
-var hideSelectors = []
-var removeSelectors = []
-var selectors = [ _____selectors_____ ]
+let label = "_____label_____"
+let route = "/_____route_____"
+let readySelector = "_____readySelector_____"
+let hideSelectors = []
+let removeSelectors = []
+let selectors = [ _____selectors_____ ]
+let hoverSelector = null
+let hoverSelectors = []
+let clickSelector = null
+let clickSelectors = []
+let postInteractionWait = 100
+let scrollToSelector = null
+let delay = _____delay_____
+let onBeforeScript = null
+let onReadyScript = "_____onReadyScriptName_____"
 
 // ---------
 // - label -
 // ---------
+// [required]
 // used on reports and screen shot file names (should be unique between tests)
 
 // ---------
 // - route -
 // ---------
+// [required]
 // the route for this test (start with a "/")
-
-// -------------
-// - selectors - (array or strings)
-// -------------
-// selectors for elements to be "captured" (CSS selector syntax)
-
-// ------------------
-// - onBeforeScript -
-// ------------------
-// Runs before each scenario
-// use for setting cookies or other env state
-// (.js suffix is optional / looks for file in "engine_scripts" dir)
-
-// -----------------
-// - onReadyScript -
-// -----------------
-// Runs after onReady event on all scenarios
-// use for simulating interactions
-// (.js suffix is optional / looks for file in "engine_scripts" dir)
 
 // -----------------
 // - readySelector -
@@ -61,6 +50,64 @@ var selectors = [ _____selectors_____ ]
 // -------------------
 // remove elements from the DOM before screen capture
 
+// -------------
+// - selectors - (array or strings)
+// -------------
+// selectors for elements to be "captured" (CSS selector syntax)
+
+// -----------------
+// - hoverSelector -
+// -----------------
+// Move the pointer over the specified DOM element prior to screen shot.
+
+// ------------------
+// - hoverSelectors - (array)
+// ------------------
+// *Puppeteer only* takes array of selectors -- simulates multiple
+// sequential hover interactions.
+
+// -----------------
+// - clickSelector -
+// -----------------
+// Click the specified DOM element prior to screen shot.
+
+// ------------------
+// - clickSelectors - (array)
+// ------------------
+// *Puppeteer only* takes array of selectors -- simulates
+// multiple sequential click interactions.
+
+// -----------------------
+// - postInteractionWait -
+// -----------------------
+// Wait for a selector after interacting with hoverSelector or clickSelector
+// (optionally accepts wait time in ms. Idea for use with a click or hover
+// element transition. available with default onReadyScript)
+
+// --------------------
+// - scrollToSelector -
+// --------------------
+// Scrolls the specified DOM element into view prior to screen shot
+// (available with default onReadyScript)
+
+// ---------
+// - delay -
+// ---------
+// Wait for x milliseconds
+
+// ------------------
+// - onBeforeScript -
+// ------------------
+// Runs before each scenario
+// use for setting cookies or other env state
+// (.js suffix is optional / looks for file in "engine_scripts" dir)
+
+// -----------------
+// - onReadyScript -
+// -----------------
+// Runs after onReady event on all scenarios
+// use for simulating interactions
+// (.js suffix is optional / looks for file in "engine_scripts" dir)
 
 
 // -------------------------------------------------------------------
@@ -70,48 +117,29 @@ module.exports = function(baseURLs) {
 	var url = (baseURLs.baseURL + route)
 	var referenceUrl = baseURLs.baseRefURL === null ? null : (baseURLs.baseRefURL + route)
 	var options = {
-		// [required] Tag saved with your reference images
-		"label": label,
-		// [required] Tag saved with your reference images
-		"url": url,
-		// Specify a different state or environment when creating reference.
-		"referenceUrl": referenceUrl,
-		// Wait until this selector exists before continuing.
-		"readySelector": readySelector,
-		// Array of selectors set to visibility: hidden
-		"hideSelectors": hideSelectors,
-		// Array of selectors set to display: none
-		"removeSelectors": removeSelectors,
-		// Array of selectors to capture. Defaults to document if omitted. Use "viewport" to capture the viewport size.
-		"selectors": selectors,
-		"selectorExpansion": true,
-		// Wait until this string has been logged to the console.
-		"readyEvent": null,
-		// Move the pointer over the specified DOM element prior to screen shot.
-		"hoverSelector": null,
-		// *Puppeteer only* takes array of selectors -- simulates multiple sequential hover interactions.
-		"hoverSelectors": null,
-		// Click the specified DOM element prior to screen shot.
-		"clickSelector": null,
-		// *Puppeteer only* takes array of selectors -- simulates multiple sequential click interactions.
-		"clickSelectors": null,
-		// Wait for a selector after interacting with hoverSelector or clickSelector (optionally accepts
-		// wait time in ms. Idea for use with a click or hover element transition. available with default onReadyScript)
-		"postInteractionWait": null,
-		// Scrolls the specified DOM element into view prior to screen shot (available with default onReadyScript)
-		"scrollToSelector": null,
-		// Wait for x milliseconds
-		"delay": delay,
-		// Percentage of different pixels allowed to pass test
-		"misMatchThreshold": 0.1,
-		"onBeforeScript": onBeforeScript,
-		// After the above conditions are met -- use this script to modify UI state prior to screen shots e.g. hovers, clicks etc.
-		"onReadyScript": onReadyScript,
-		// If set to true -- any change in selector size will trigger a test failure.
-		"requireSameDimensions": false
+		"label": label,									// [required] Tag saved with your reference images
+		"url": url,										// [required] Tag saved with your reference images
+		"referenceUrl": referenceUrl,					// Specify a different state or environment when creating reference.
+		"readySelector": readySelector,					// Wait until this selector exists before continuing.
+		"hideSelectors": hideSelectors,					// Array of selectors set to visibility: hidden
+		"removeSelectors": removeSelectors,				// Array of selectors set to display: none
+		"selectors": selectors,							// Array of selectors to capture. Defaults to document if omitted. Use "viewport" to capture the viewport size.
+		"selectorExpansion": true,						// If you want BackstopJS to find and take screenshots of all matching selector instances then set to true.
+		"readyEvent": null,								// Wait until this string has been logged to the console.
+		"hoverSelector": hoverSelector,					// Move the pointer over the specified DOM element prior to screen shot.
+		"hoverSelectors": hoverSelectors,				// *Puppeteer only* takes array of selectors -- simulates multiple sequential hover interactions.
+		"clickSelector": clickSelector,					// Click the specified DOM element prior to screen shot.
+		"clickSelectors": clickSelectors,				// *Puppeteer only* takes array of selectors -- simulates multiple sequential click interactions.
+		"postInteractionWait": postInteractionWait,		// Wait for a selector after interacting with hoverSelector or clickSelector (optionally accepts wait time in ms. Idea for use with a click or hover element transition. available with default onReadyScript)
+		"scrollToSelector": scrollToSelector,			// Scrolls the specified DOM element into view prior to screen shot (available with default onReadyScript)
+		"delay": delay,									// Wait for x milliseconds	
+		"misMatchThreshold": 0.1,						// Percentage of different pixels allowed to pass test
+		"onBeforeScript": onBeforeScript,				// Used to set up browser state e.g. cookies.
+		"onReadyScript": onReadyScript,					// After the above conditions are met -- use this script to modify UI state prior to screen shots e.g. hovers, clicks etc.
+		"requireSameDimensions": false					// If set to true -- any change in selector size will trigger a test failure.
 	}
 
-	if(baseURLs.baseRefURL === null) {
+	if (baseURLs.baseRefURL === null) {
 		delete options.referenceUrl
 	}
 
